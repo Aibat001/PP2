46c3094ececab41167b27f6c0af461bbd82d78ea
Aibat001 <157606646+Aibat001@users.noreply.github.com>
2024-01-24 20:58:35 +0600

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,38 @@
+# ex1
+x = "Hello World"
+print(len(x))
+
+# ex2
+txt = "Hello World"
+x = txt[0]
+print(x)
+
+# ex3
+txt = "Hello World"
+x = txt[2:5]
+print(x)
+
+# ex4
+txt = " Hello World "
+x = txt.strip()
+print(x)
+
+# ex5
+txt = "Hello World"
+txt = txt.upper()
+print(txt)
+
+# ex6
+txt = "Hello World"
+txt = txt.lower()
+print(txt)
+
+# ex7
+txt = "Hello World"
+txt = txt.replace("H", "J")
+print(txt)
+
+# ex8
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
---
+++ variables.py
@@ -0,0 +1,27 @@
+# ex1
+carname = "Volvo"
+
+# ex2
+x = 50
+
+# ex3
+x = 5
+y = 10
+print(x + y)
+
+# ex4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+# ex5
+myfirst_name = "John"
+
+# ex6
+x = y = z = "Orange"
+
+# ex7
+def myfunc():
+    global x
+    x = "fantastic"
